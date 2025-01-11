e194b6c2c883060ddf476a106b6c81b5ebc37eb3
ziarul islam <mdziarulislam.bu.7@gmail.com>
2025-01-11 10:12:36 +0600

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 year =int(input('enter the value:'))
 if ((year%400==0)or (year%4==0 and year%100!=0)):
-    print('year is leap year')
+    print(f'{year} is leap year')
 else:
-    print('year is not leap year')
+    print(f'{year} is not leap year')
